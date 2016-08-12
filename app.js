7623fbf3cd8697f959e54a675873e12c9a2c47d8
Simeon Lee <simeonhlee@gmail.com>
2016-08-12 13:37:55 -0700

    Implement functionality in app.js

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+// var onListItemClick = (event) => {
+//    console.log('I got clicked');
+// };
+
+var App = () => (
+  <div>
+		<h1>Grocery List</h1>
+  	<GroceryList list={['Bananas', 'Carrots', 'Oranges']} />
+  </div>
+);
+
+var GroceryList = (props) => (
+	<ul>
+		{props.list.map(item => <GroceryListItem item={item} />)}
+	</ul>
+)
+
+class GroceryListItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			done: false
+		};
+	}
+	onListItemClick() {
+		this.setState({
+			done: !this.state.done
+		});
+	}
+	render() {
+		var style = {
+			textDecoration: this.state.done ? 'line-through' : 'none'
+		}
+		return (
+			<li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+		);
+	}
+} 
+
+var Bananas = () => (
+	<li>bananas</li>
+)
+
+var Carrots = () => (
+	<li>carrots</li>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
